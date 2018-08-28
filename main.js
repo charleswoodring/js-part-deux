e090cd9f56c7b9bfadbf2f0808e202f019b83ba1
Charles Woodring <charles.s.woodring@gmail.com>
2018-08-27 19:18:17 -0500

    finish strings

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
+const imaString = 'hi';
+
+const age = 99;
+    if (age < 21) {
+        console.log('no drinks for you');
+    } else {
+        console.log('come on in');
+    }
+
+
+
+
+
+const a = 1;
+const b = '1';
+a === b // false  eq
+a == b // true  eq
+a !== b // true ne
+a != b // false ne
+
+const favoriteColor = 'blue';
+const favoriteAnimal = 'bird';
+
+if(favoriteAnimal ==='bird' && favoriteColor === 'blue){
+    console.log('welcome to the club');
+} else if (favoriteAnimal === 'cat' || favoriteColor === 'red'){
+    console.log('$5000 to join')
+} else console.log('go away');
+}
+
+const hiDiv = document.getElementById('hi');
+console.log(hiDiv);
+
+const pie = 'peach';
+console.log('pie length: ', pie.length);
+console.log('#3', pie.charAt(2));
+console.log('indexof cat' , pie.indexOf)'cat'));
+
 console.log('hello there');
+
+
